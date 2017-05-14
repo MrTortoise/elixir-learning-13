e9497c7e869b5f7fb42e3da513fdeb7621332b98
John Nicholas <john.nicholas@kynesic.com>
2017-05-14 22:24:25 +0100

    span function

--- list-and-recursion/span.exs
+++ list-and-recursion/span.exs
@@ -4,4 +4,14 @@ defmodule Span do
     [from | span(from+1,to)]
   end
 
+  def primeNumbers(n) do
+    for x <- span(2, n), isPrime(n), do: x
+  end
+
+  def isPrime(2), do: true
+  def isPrime(3), do: true
+  def isPrime(n) do
+    Enum.reduce(span(2,n-1), true, fn val, isPrime ->  isPrime && rem(n,val) != 0 end )
+  end
+
 end
