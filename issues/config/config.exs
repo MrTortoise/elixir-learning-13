d4fc6233539ad7e559af55beff286a683996ba9c
John Nicholas <john.nicholas@kynesic.com>
2017-05-20 21:30:03 +0100

    started to add logging. but also hooked up main

--- issues/config/config.exs
+++ issues/config/config.exs
@@ -22,6 +22,7 @@ use Mix.Config
 #
 
 config :issues, github_url: "https://api.github.com"
+config :logger, compile_time_purge_level: :info
 import_config "#{Mix.env}.exs"
 
 
--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -9,10 +9,11 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project.
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
+    |> IO.puts
   end
 
   @doc """
