619f1bab05156ed4c04f417282c2c7ef73e58832
John Nicholas <themrtortoise@gmail.com>
2017-06-18 21:51:38 +0100

    removed redundant initialise

--- sequence/lib/sequence/stack.ex
+++ sequence/lib/sequence/stack.ex
@@ -1,10 +1,6 @@
 defmodule Sequence.Stack do
   use GenServer
 
-  def handle_call({:initialise,stack}, _from, _current_stack) do
-    {:reply, :initialised, stack}
-  end
-
   def handle_call(:pop, _from, stack) do
     [head|tail] = stack
     {:reply, head, tail}
